1d954ab035cfa8983190002e20c466863f9b7bd8
Edward Banner <edward@Edwards-Air.localdomain>
2025-08-06 16:54:08 -0400

    Make POST endpoint

--- api/index.py
+++ api/index.py
@@ -1,10 +1,27 @@
+import json
+
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def do_POST(self):
+        content_length = int(self.headers.get('Content-Length', 0))
+        post_data = self.rfile.read(content_length)
+
+        # Optionally parse JSON body
+        try:
+            data = json.loads(post_data)
+        except json.JSONDecodeError:
+            data = {"error": "Invalid JSON"}
+
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+
+        response = {
+            "message": "POST request received!",
+            "data": data
+        }
+
+        self.wfile.write(json.dumps(response).encode('utf-8'))
         return
